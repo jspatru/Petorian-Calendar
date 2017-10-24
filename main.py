01e8e431043c0eebc8cb406aa8f7fce458f56085
Jack Patru <jspatru@gmail.com>
2017-10-24 12:52:42 -0400

    Create main.py
    Python code

---
+++ main.py
@@ -0,0 +1,9 @@
+#Every variable is measured in an SI second
+#
+import math
+secondsInADay = 86164.0916 #23.9344699 hours or 0.99726958 mean solar days
+secondsInAYear = 31558149.504 #All Sidereal
+sdaysInAYear = secondsInAYear/secondsInADay #366.256394259
+epoch = secondsInAYear/73 #432303.417863, one sidereal year divided by 73
+print part
+#5.01721088026 parts are in an epoch
