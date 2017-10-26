36d5eb8f1785a5a9f47db0da9cae663a0e945d39
Jack Patru <jspatru@gmail.com>
2017-10-26 14:02:01 -0400

    Update revision.py

--- revision.py
+++ revision.py
@@ -1,16 +1,21 @@
 #Every variable is measured in an SI second
 #
-import math
 secondsInADay = 86164.0916 #23.9344699 hours or 0.99726958 mean solar days
 secondsInAYear = 31558149.5 #All Sidereal
 sdaysInAYear = secondsInAYear/secondsInADay #366.256394259
 epoch = secondsInAYear/73 #432303.417808, one sidereal year divided by 73
 #5.01721088026 parts are in an epoch
-part = 432303.417808/5.01721088026
-p=4
-e=72
-lc1=0
-lc2=0
-while p >= 4:
-    print part*p
-    p=p+1
+part = epoch/5 #86460.6835616
+p=0
+e=0
+lc1=1
+lc2=1
+while e <= 72:
+  print("Epoch " + str(lc1) + ": " + str(epoch*e))
+  e=e+1
+  lc1=lc1+1
+  p=0
+  while p <= 4:
+      print("\tPart " + str(p+1) + ": " + str(part*lc2))
+      p=p+1
+      lc2=lc2+1
