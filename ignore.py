b631f814dac8ddea9f1e2e1b4bf78604aa7f081a
Jack Patru <jspatru@gmail.com>
2017-10-26 14:52:52 -0400

    Update ignore.py

--- ignore.py
+++ ignore.py
@@ -1,5 +1,5 @@
 #Every variable is measured in an SI second
-#
+#This files exists just for archival purposes. If you got here by mistake, go back and view main.py
 import math
 secondsInADay = 86164.0916 #23.9344699 hours or 0.99726958 mean solar days
 secondsInAYear = 31558149.504 #All Sidereal
@@ -7,3 +7,8 @@ sdaysInAYear = secondsInAYear/secondsInADay #366.256394259
 epoch = secondsInAYear/73 #432303.417863, one sidereal year divided by 73
 print part
 #5.01721088026 parts are in an epoch
+
+#This is an incomplete full version that was last edited aside from this comment two parts ago. In this original file 
+#the amount of seconds I got in a year was off by a margin of .04 seconds. And since I made this draft, the amount of 
+#parts in an epoch has been changed from 5.01721088026 to just five. And lastly, the length of an epoch has been changed 
+#from 432303.417863 seconds to 432303.417808 seconds because the original figure was off by .000055 seconds.
