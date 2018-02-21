9133997e25ad5e6fa6867b015422f36a7fc4e774
Jack Patru <jspatru@gmail.com>
2018-02-21 00:54:33 -0500

    Update dates.py
    The program now tells you the date on its own. Change the 9997 to whatever you want for whatever year you want to make it. (whatever year it is + 9997 = your year). New partly time system coming soon!

--- dates.py
+++ dates.py
@@ -1,39 +1,42 @@
 #Every variable is measured in an SI second
 #As of January 1, 2017 CE, we were about 29.691992871777067 parts behind schedule.
 import math
-from datetime import *
 import time
-import calendar
+from datetime import *
 secondsInADay = 86164.0916 #23.9344699 hours or 0.99726958 mean solar days
 secondsInAYear = 31558149.5 #All Sidereal
 ephemerisDay = 86400
+z = datetime.now()
+theYear = z.year
 sdaysInAYear = secondsInAYear/secondsInADay #366.256394259
 epoch = secondsInAYear/73 #432303.417808, one sidereal year divided by 73
-delay = 2523249.324
+delay = 2523249.324*1
 #5.01721088026 parts are in an epoch
 part = epoch/5 #86460.6835616, or 24.01685654488889 hours
+p=0
 e=0
 lc1=1
 lc2=1
-now = datetime.now()
-d = date(datetime.now().year, 01, 01)
-bang = calendar.timegm(d.timetuple())
+today = datetime.now()
+NewYear=datetime.fromtimestamp(epoch*e-delay+1483228800-18000+secondsInAYear*(theYear-2017))
+secondsSinceNewYear=today-NewYear
 while e <= 72:
-  p=0
-  print("Epoch " + str(lc1) + ": " + str(datetime.fromtimestamp(epoch*e-delay+bang)) + " UTC") #946684800 
-  e=e+1
-  lc1=lc1+1
-  while p <= 4:
-    #print datetime.datetime(2000, 01, 01)
-    #print (datetime.fromtimestamp(part*(lc2-1)-delay).strftime("%A, %B %d %I:%M:%S"))
-    print("\tPart " + str(p+1) + ": " + str(datetime.fromtimestamp(part*(lc2-1)-delay+bang)) + " UTC")
-    p=p+1
-    lc2=lc2+1
-    #.strftime("%Y, %A, %B %d %I:%M:%S") for the date spelled out
+	x=datetime.fromtimestamp(epoch*e-delay+1483228800-18000+secondsInAYear*(theYear-2017))
+	print("Epoch " + str(lc1) + ": " + str(x) + " EST")
+	p=0
+	e=e+1
+	lc1=lc1+1
+	while p <= 4:
+		y=datetime.fromtimestamp(part*(lc2-1)-delay+1483228800-18000+secondsInAYear*(theYear-2017))#.strftime("%A, %B %d %I:%M:%S") str(part*(lc2-1)-delay)
+		print("\tPart " + str(p+1) + ": " + str(y) + " EST")
+		p=p+1
+		if NewYear+secondsSinceNewYear >= y:
+			todaysPart=p
+			todaysEpoch=lc1
+		lc2=lc2+1
+print("Today is "+str(todaysPart)+"."+str(todaysEpoch-1)+"."+str(theYear+9997)+".")
 #print("\tYEAR END: " + str(secondsInAYear-delay))
 #2567190 seconds
 #29 days, 4 hours, 54 minutes, 9.324000000022352 seconds; 2523249.324 seconds
-#510 leap days since January 1, 45 BCE
-#http://keisan.casio.com/exec/system/1295254517 <-- this link helped a lot
-#365 days, 06 hours, 09 minutes, 09.5 seconds.
-#65072904269-65070435024=2469245 (28.57922454 days)
+
+#3:57 PM, January 11, 2018 CE (Gregorian Calendar): The program has officially just corrected me about what part it is. I'm so proud.
